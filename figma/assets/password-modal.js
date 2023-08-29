a252e21c4efae680941bc486a9c45e1d5b07e28e
HoangThiPhuongThanh <106575031+HoangThiPhuongThanh@users.noreply.github.com>
2023-08-29 09:56:57 +0700

    ok

--- figma/assets/password-modal.js
+++
@@ -1,9 +0,0 @@
-var passwordmodal = document.querySelector('.password__modal'); 
-var passwordmodalopen = document.querySelector('.password__modal-open');
-var closeBtn = document.querySelector('.modal__close-button'); 
-passwordmodalopen.addEventListener('click', () => {
-  passwordmodal.classList.add('open');
-});
-closeBtn.addEventListener('click', () => {
-  passwordmodal.classList.remove('open');
-});
